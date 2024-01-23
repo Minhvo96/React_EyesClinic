9e3a0ae4847198095516fea02395f1f624b8ff0b
Minhvo96 <voquangminhbck38@gmail.com>
2024-01-23 16:49:19 +0700

    nothing to commit

--- src/components/waitingList/WaitingList.jsx
+++ src/components/waitingList/WaitingList.jsx
@@ -7,7 +7,7 @@ import { Link } from 'react-router-dom';
 export default function WaitingList() {
 
     useEffect(() => {
-        UseEffectForRender()
+        UseEffectForRender();
     }, []);
 
     return (
